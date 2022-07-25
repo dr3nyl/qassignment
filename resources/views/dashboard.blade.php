e800bfb8cc857e5b7286919baf457eda1a9cf128
Drenyl <drenyltobi@gmail.com>
2022-07-25 21:43:48 +0800

    Display authors in table layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,11 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                <table class="table-auto">
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Birthday</th>
+                        <th>Gender</th>
+                        <th>Place of birth</th>
+                        <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     @foreach($authors as $author)
 
-                        {{ $author->id }}
+                        <tr>
+                            <td>{{ $author->first_name.' '.$author->last_name}}</td>
+                            <td>{{ $author->birthday }}</td>
+                            <td>{{ $author->gender }}</td>
+                            <td>{{ $author->place_of_birth }}</td>
+                            <td></td>
+                        </tr>
 
                     @endforeach
+                        
+                    </tbody>
+                </table>
+                    
                 </div>
             </div>
         </div>
