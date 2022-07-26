35b9ae090b30025b14ab04a0424b95f6efcfeae5
Drenyl <drenyltobi@gmail.com>
2022-07-26 22:22:01 +0800

    Add flowbite datepicker cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,11 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" 
+                integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" 
+                crossorigin="anonymous">
+        </script>
+        <script src="https://unpkg.com/flowbite@1.5.1/dist/datepicker.js"></script>
         
     </head>
     <body class="font-sans antialiased">
